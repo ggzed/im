0103ff5d534183b4c660b9e46b0b8ab65adeb7b2
ggzed <1500510117@qq.com>
2024-03-22 18:02:07 +0800

    添加移动项目

---
+++ im-mobile/src/components/shelf/BookCell.vue
@@ -0,0 +1,28 @@
+<template>
+  <van-swipe-cell>
+    <van-card
+        num="2"
+        price="2.00"
+        desc="描述信息"
+        title="商品标题"
+        class="goods-card"
+        thumb="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg"
+    />
+    <template #right>
+      <van-button square text="删除" type="danger" class="delete-button" />
+    </template>
+  </van-swipe-cell>
+</template>
+<script setup lang="ts">
+
+</script>
+<style scoped>
+.goods-card {
+  margin: 0;
+  background-color: white;
+}
+
+.delete-button {
+  height: 100%;
+}
+</style>
